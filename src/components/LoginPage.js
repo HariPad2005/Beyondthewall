2e6e2a2af012f5a247e233d09457bf00dfbf03e6
Hariharan Padmanaban <hariharanbindu2019@gmail.com>
2024-09-24 10:32:34 +0530

    minor changes

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,12 +1,39 @@
-// LoginPage.js
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAvatar } from './AvatarContext';
 import './LoginPage.css';
 
 const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const navigate = useNavigate();
   const { selectedAvatar } = useAvatar();
+
+  const handleLogin = async () => {
+    const loginData = { username, password };
+
+    try {
+      const response = await fetch('/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(loginData),
+      });
+
+      const data = await response.json();
+
+      if (data.success) {
+        // Handle successful login (e.g., navigate to a different page)
+        navigate('/timer');
+      } else {
+        // Handle login failure (e.g., display error message)
+        alert(data.message);
+      }
+    } catch (error) {
+      console.error('Error during login:', error);
+    }
+  };
 
   return (
     <div className="login-page">
@@ -22,11 +49,23 @@ const LoginPage = () => {
           </div>
         )}
         <h1>Enter the Realm</h1>
-        <input type="text" placeholder="Username" className="username-input" />
-        <input type="password" placeholder="Password" className="password-input" />
+        <input
+          type="text"
+          placeholder="Username"
+          className="username-input"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="password-input"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <div className="buttons">
           <button onClick={() => navigate('/select-avatar')} className="avatar-button">Select Avatar</button>
-          <button onClick={() => navigate('/timer')} className="login-button">Begin Your Quest</button>
+          <button onClick={handleLogin} className="login-button">Begin Your Quest</button>
         </div>
       </div>
     </div>
